a5f0f80856769295d95fd057dc295b1cd9592858
If Pol <68429746+ifpolwebdev@users.noreply.github.com>
2022-07-10 18:24:34 +0300

    create set interval method to make countdown dynamic

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const countdown = document.querySelector(".countdown");
 
-const deadline = new Date(2022, 7, 15, 12, 00, 00);
+const interval = setInterval(() => {
+    const deadline = new Date(2022, 8, 10, 12, 00, 00);
 
 const current = new Date();
+
+const diff = deadline - current;
+
+const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+
+const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+
+const minutes = Math.floor((diff / (1000 * 60) % 60));
+
+const seconds = Math.floor((diff / 1000) % 60);
+
+countdown.innerHTML = `
+    <div>${days}</div>
+    <div>${hours}</div>
+    <div>${minutes}</div>
+    <div>${seconds}</div>    
+`;
+}, 1000);
+
