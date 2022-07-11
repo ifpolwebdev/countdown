7bb19f683d3243d5d4dbb5930b6f78583384e4b9
If Pol <68429746+ifpolwebdev@users.noreply.github.com>
2022-07-11 07:36:15 +0300

    add interactivity to countdown and complete changes in js

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
 const countdown = document.querySelector(".countdown");
 
 const interval = setInterval(() => {
-    const deadline = new Date(2022, 8, 10, 12, 00, 00);
+    const deadline = new Date(2022, 11, 25, 12, 00, 00);
 
 const current = new Date();
 
 const diff = deadline - current;
 
-const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+const days = Math.floor(diff / (1000 * 60 * 60 * 24)) + "";
 
-const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+const hours = Math.floor((diff / (1000 * 60 * 60)) % 24) + "";
 
-const minutes = Math.floor((diff / (1000 * 60) % 60));
+const minutes = Math.floor((diff / (1000 * 60)) % 60) + "";
 
-const seconds = Math.floor((diff / 1000) % 60);
+const seconds = Math.floor((diff / 1000) % 60) + "";
 
 countdown.innerHTML = `
-    <div data-content="Days">${days}</div>
-    <div data-content="Hours">${hours}</div>
-    <div data-content="Minutes">${minutes}</div>
-    <div data-content="Seconds">${seconds}</div>    
+    <div data-content="Days">${days.length === 1 ? `0${days}` : days}</div>
+    <div data-content="Hours">${hours.length === 1 ? `0${hours}` : hours}</div>
+    <div data-content="Minutes">${minutes.length === 1 ? `0${minutes}` : minutes}</div>
+    <div data-content="Seconds">${seconds.length === 1 ? `0${seconds}` : seconds}</div>    
 `;
+
+if(diff < 0) {
+    clearInterval(interval);
+    countdown.innerHTML = "<h1>Here We Go!!!</h1"
+}
+
+document.querySelector(".reset").addEventListener('click', () => {
+    clearInterval(interval);
+
+    const divs = document.querySelectorAll('.countdown div');
+
+    divs.forEach(div => {
+        div.innerHTML =  '00';
+    })
+})
+
 }, 1000);
 
